025ae263e8314e2e4d8e4d422a2f41b7d97603d2
Carlos Fernando Avila Gratz <cafe@q1software.com>
2013-05-08 18:01:09 -0300

    forgot to comit LibXML.pm o.O

--- lib/Web/Query/LibXML.pm
+++ lib/Web/Query/LibXML.pm
@@ -2,8 +2,30 @@ package Web::Query::LibXML;
 use 5.008005;
 use strict;
 use warnings;
+use parent qw/Web::Query Exporter/;
+use HTML::TreeBuilder::LibXML;
+
 
 our $VERSION = "0.01";
+
+our @EXPORT = qw/wq/;
+
+sub wq { Web::Query::LibXML->new(@_) }
+
+sub _build_tree {
+    my ($self, $content) = @_;    
+    my $tree = HTML::TreeBuilder::LibXML->new();
+    $tree->ignore_unknown(0);
+    $tree->store_comments(1);
+    $tree;    
+}
+
+
+
+
+
+
+
 
 
 
@@ -14,7 +36,7 @@ __END__
 
 =head1 NAME
 
-Web::Query::LibXML - It's new $module
+Web::Query::LibXML - drop-in replacement for Web::Query, based on XML::LibXML
 
 =head1 SYNOPSIS
 
